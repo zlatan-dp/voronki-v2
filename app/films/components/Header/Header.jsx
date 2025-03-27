da362418dad58f20bd4137c8188e42449675b861
zlatandp <zlatandnepr@gmail.com>
2025-03-27 14:01:14 +0200

    fixed css in Films - Order Info pages (all flows)

--- app/films/components/Header/Header.jsx
+++ app/films/components/Header/Header.jsx
@@ -1,15 +1,18 @@
-import styles from "./Header.module.css"
-import Image from "next/image"
+import styles from "./Header.module.css";
+import Image from "next/image";
 
 export default function Header() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.logoWrap}>
-                <Image src="/images/kivi-logo.svg"
-                        alt="logo"
-                        width={74}
-                        height={24}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoWrap}>
+        <Image
+          src="/images/kivi-logo.svg"
+          alt="logo"
+          width={74}
+          height={24}
+          priority
+        />
+      </div>
+    </div>
+  );
 }
