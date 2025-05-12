38999c88bb4afd00ca246b27368ead4f4aea0bb2
zlatandp <zlatandnepr@gmail.com>
2025-05-12 19:12:24 +0300

    finished protectie section

--- app/films/componentsV3/Protective/Protective.jsx
+++ app/films/componentsV3/Protective/Protective.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Swiper, SwiperSlide} from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -10,29 +10,32 @@ import SectionTitle from "../sectionTitle/sectionTitle";
 import Button from "../button/button";
 import { ProtectiveData } from "./ProtectiveData";
 
-export default function Protective({href}) {
-     return (
-        <div className={styles.protectiveContainer}>
-            <SectionTitle align="center">4 Schutzschichten</SectionTitle>
-            <Swiper
-                modules={[Pagination]}
-                slidesPerView={1.2}
-                spaceBetween={15}
-                centeredSlides={true}
-                pagination={{ clickable: true }}
-                className={styles.slider}
-                >
-                    {ProtectiveData.map(({id, subTitle, text, bgImage}) => (
-                         <SwiperSlide key={id} className={styles.slide} style={{ backgroundImage: `url(${bgImage})`}}>
-                            <div className={styles.infoWrap}>
-                                <p className={styles.subTitle}>{subTitle}</p>
-                                <p>{text}</p>
-                            </div>
-                         </SwiperSlide>
-                    ))}
-                </Swiper>
+export default function Protective({ href }) {
+  return (
+    <div className={styles.protectiveContainer}>
+      <SectionTitle align="center">Schutzschichten</SectionTitle>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        spaceBetween={15}
+        centeredSlides={true}
+        pagination={{ clickable: true }}
+        className={styles.slider}
+      >
+        {ProtectiveData.map(({ id, subTitle, text }) => (
+          <SwiperSlide key={id} className={styles.slide}>
+            <div className={styles.infoWrap}>
+              <p className={styles.number}>{id}</p>
+              <p className={styles.subTitle}>{subTitle}</p>
+              <p className={styles.text}>{text}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-            <Button href={href} dataBlock={'Protective section'}>JETZT SCHUTZ WÄHLEN</Button>
-        </div>
-    );
+      <Button href={href} dataBlock={"Protective section"}>
+        Jetzt kaufen
+      </Button>
+    </div>
+  );
 }
--- app/films/flow-31/page.jsx
+++ app/films/flow-31/page.jsx
@@ -6,11 +6,12 @@ import LandingWrap from "../componentsV3/LandingWrap/LandingWrap";
 import HeroSection from "../componentsV3/HeroSection/HeroSection";
 import DiscountSection from "../componentsV3/Discount/Discount";
 import Realiability from "../componentsV3/Reliability/Reliability";
-
 import FilmsInfo from "../componentsV3/FilmsInfo/FilmsInfo";
+
+import Protective from "../componentsV3//Protective/Protective";
+
 import EasyInstallSection from "../componentsV3/EasyInstall/EasyInstall";
 import VideoSection from "../componentsV3/Video/Video";
-import Protective from "../componentsV3//Protective/Protective";
 import Faq from "../componentsV3/Faq/Faq";
 import Feedback from "../componentsV3/Feedback/Feedback";
 import Explanation from "../componentsV3/Explanation/Explanation";
@@ -29,12 +30,11 @@ export default function filmsLanding() {
         <HeroSection href={`/films/${currentFlow}/choose-film`} />
         <DiscountSection href={`/films/${currentFlow}/choose-film`} />
         <Realiability href={`/films/${currentFlow}/choose-film`} />
-
         <FilmsInfo href={`/films/${currentFlow}/choose-film`} />
+        <Protective href={`/films/${currentFlow}/choose-film`} />
 
         <EasyInstallSection href={`/films/${currentFlow}/choose-film`} />
         <VideoSection href={`/films/${currentFlow}/choose-film`} />
-        <Protective href={`/films/${currentFlow}/choose-film`} />
         <Faq href={`/films/${currentFlow}/choose-film`} />
         <Feedback />
         <Explanation
