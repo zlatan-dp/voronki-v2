f2419dabadef764b3062806ac0fc56216b8c7502
oleksandr shubin <zlatandnepr@gmail.com>
2025-12-30 18:23:04 +0200

    enabled fb script

--- app/sprints/flow-05/components/ChoosePlan/ChoosePlan.jsx
+++ app/sprints/flow-05/components/ChoosePlan/ChoosePlan.jsx
@@ -54,7 +54,7 @@ export default function ChoosePlanComponent() {
   const handleSubmit = async () => {
     if (!selectedPlanId) return;
 
-    // await fbq("track", "AddToCart");
+    await fbq("track", "AddToCart");
 
     const payload = planPayload(selectedPlanId);
     await goToNextStep(payload);
