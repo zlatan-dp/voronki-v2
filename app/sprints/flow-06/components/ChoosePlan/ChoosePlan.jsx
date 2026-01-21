fea66a7a438e5ba0a5c9d99b4e1f03a56c6b0305
oleksandr shubin <zlatandnepr@gmail.com>
2026-01-21 18:06:26 +0200

    corrected chooseplan, flow-06

--- app/sprints/flow-06/components/ChoosePlan/ChoosePlan.jsx
+++ app/sprints/flow-06/components/ChoosePlan/ChoosePlan.jsx
@@ -109,7 +109,7 @@ export default function ChoosePlanComponent() {
       <p className={styles.discountedText}>
         Discounted price applies to your first subscription. Your subscription
         will automatically renew at full price of{" "}
-        {selectedPlanId === 4 ? "$14.99" : "$9.99"} per month at the end of the
+        {selectedPlanId === 1 ? "$14.99" : "$9.99"} per month at the end of the
         chosen subscription period until you cancel in your account.
       </p>
     </BlockWrap>
