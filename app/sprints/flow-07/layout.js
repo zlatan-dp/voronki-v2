ec780f351b7d187051e792ddabe445f5a2320398
oleksandr shubin <zlatandnepr@gmail.com>
2026-02-11 14:42:15 +0200

    flow07 - added FBpixel to DP

--- app/sprints/flow-07/layout.js
+++ app/sprints/flow-07/layout.js
@@ -1,5 +1,12 @@
 import { TimerProvider } from "../actions/useTimerContext";
 
+import FBPixelDigitalPack from "@/app/components/FacebookPixel/FbPixelDigitalPack";
+
 export default function Flow06Layout({ children }) {
-  return <TimerProvider initialSeconds={600}>{children}</TimerProvider>;
+  return (
+    <TimerProvider initialSeconds={600}>
+      <FBPixelDigitalPack />
+      {children}
+    </TimerProvider>
+  );
 }
