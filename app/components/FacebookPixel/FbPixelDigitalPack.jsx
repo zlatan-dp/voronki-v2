15411da8832131a3294e0c1c059559d430ac84a3
oleksandr shubin <zlatandnepr@gmail.com>
2026-02-11 14:54:49 +0200

    FB DP pixel - fixed

--- app/components/FacebookPixel/FbPixelDigitalPack.jsx
+++ app/components/FacebookPixel/FbPixelDigitalPack.jsx
@@ -10,9 +10,10 @@ export default function FBPixelDigitalPack() {
           __html: `
             !function(f,b,e,v,n,t,s){
                 if(f.fbq)return;
-                n=f.fbq=function(){n.callMethod?
+                n=f.fbq=function(){n.callMethod ?
                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;
+                if(!f._fbq)f._fbq=n;
+                n.push=n;
                 n.loaded=!0;
                 n.version='2.0';
                 n.queue=[];
